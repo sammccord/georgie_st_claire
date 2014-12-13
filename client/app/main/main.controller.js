8911e7816f3ea1c87b34a6952bb9f4e590f769cb
Sam McCord <samuel.e.mccord@gmail.com>
2014-12-13 14:54:18 -0500

    new bower, some main route frontend

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,18 +3,18 @@
 angular.module('georgieStClaireApp')
     .controller('MainCtrl', function($scope, $http, socket, speak) {
 
-        speak('testing testing one two three',function(){
-        	console.log('done');
+        speak('testing testing one two three', function() {
+            console.log('done');
         });
 
         socket.syncUpdates('speak_news', null, function(e, data) {
-        		console.log(e);
+            console.log(e);
             console.log(data);
             speak(data);
         });
 
-        $scope.post = function(){
-        	console.log('stuff');
+        $scope.post = function() {
+            console.log('stuff');
         }
 
         $scope.addThing = function() {
@@ -30,4 +30,46 @@ angular.module('georgieStClaireApp')
         $scope.$on('$destroy', function() {
             socket.unsyncUpdates('speak_news');
         });
+
+        $scope.today = function() {
+            $scope.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.clear = function() {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[1];
+
+        $scope.get_news = function() {
+            console.log($scope.dt);
+            $http.post('/api/news/', {
+                name: $scope.dt
+            });
+        }
+
     });
