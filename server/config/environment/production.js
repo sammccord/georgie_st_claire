acef03257177daaa0083b89ed8c630e87f408adc
Sam McCord <samuel.e.mccord@gmail.com>
2014-12-14 15:16:23 -0500

    keys

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -13,13 +13,13 @@ module.exports = {
             process.env.PORT ||
             8080,
 
-  nyt: process.env.TIMES,
+nyt: 'd6063f8494f02a6bca958b5de50d7646:3:70396827',
 
-  twit: {
+    twit: {
         consumer_key: 's66nH2xRSBvwVm4thYCgI0TiI',
-        consumer_secret: process.env.TWIT_CONSUMER,
+        consumer_secret: 'UBjO8gQkuHERCkhSu6O6oEBWVjvgx8AWycJmeb55QBmMdS2E1m',
         access_token: '2919106498-Mhb84uKJ0r0vCKsYR4X0xwMv2eUK6qNVYwvlPIx',
-        access_token_secret: process.evn.TWIT_SECRET
+        access_token_secret: 'AxWa5heUGN5uPIkXujM5A7OfgWnhFt2wi4Pi7sLT0RLvO'
     },
 
   // MongoDB connection options
