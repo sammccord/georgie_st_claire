94770c35b0272a6bce22bacf7b072c2e4fe77b8c
Sam McCord <samuel.e.mccord@gmail.com>
2014-12-15 00:40:05 -0500

    Browser check

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,40 +3,54 @@
 angular.module('georgieStClaireApp')
     .controller('NavbarCtrl', function($scope, $location, Auth, $modal, $log) {
 
+        var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+        if (!is_chrome) {
+            var openBrowser = function(size) {
+                var modalInstance = $modal.open({
+                    templateUrl: 'wrongbrowser.html',
+                    controller: 'ModalInstanceCtrl',
+                    size: size,
+                    resolve: {
+                        items: function() {
+                            return $scope.items;
+                        }
+                    }
+                });
+            };
+            openBrowser();
+        }
+
         $scope.items = ['item1', 'item2', 'item3'];
 
-  $scope.openModal = function (size) {
+        $scope.openModal = function(size) {
 
-    var modalInstance = $modal.open({
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                size: size,
+                resolve: {
+                    items: function() {
+                        return $scope.items;
+                    }
+                }
+            });
 
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
+            modalInstance.result.then(function(selectedItem) {
+                $scope.selected = selectedItem;
+            }, function() {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
 
     }).controller('ModalInstanceCtrl', function($scope, $modalInstance, items) {
 
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
+        $scope.items = items;
 
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
+        $scope.ok = function() {
+            $modalInstance.close($scope.selected.item);
+        };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-});
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        };
+    });
